8b53f515c7bec5a853b309e4349507acaddfd1af
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-04-02 13:48:19 +0530

    added firebase files

---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAD-b522_pyUSL_t2nl71oNOfCVRoljF0w',
+  authDomain: 'netflix-clone-9fd7e.firebaseapp.com',
+  projectId: 'netflix-clone-9fd7e',
+  storageBucket: 'netflix-clone-9fd7e.appspot.com',
+  messagingSenderId: '441633862911',
+  appId: '1:441633862911:web:54cc328a829b94b7b288ca',
+}
